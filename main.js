f3ed547b47dbe45158a1386dd5ed0a9d33de4676
Abdul Rafay <rafay.busi@gmail.com>
2025-03-06 17:06:22 +0500

    added text file

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const http = require('http')
+const http = require("http");
 
-const server = http.createServer((req,res)=> {
-    res.write('welcome to our website')
-    res.end()
-})
-
-server.listen(3000)
+const server = http.createServer((req, res) => {
+  res.write("welcome to our website");
+  res.end();
+});
+console.log();
+server.listen(3000);
